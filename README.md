c7bd2ad0f3d9aa131871850c6bb8f616ea815619
SIN88 <KG19882020@GMAIL.COM>
2021-05-30 16:55:48 -0700

    going over older homeworks to submit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 04-web-api
+
+in this project I am going to make a simple web api game .
---
+++ script.js
@@ -0,0 +1,26 @@
+var start button = document.querySelector(start btn)
+var start button = document.querySelector(start btn)
+var start button = document.querySelector(start btn)
+var start button = document.querySelector(start btn)
+var start button = document.querySelector(start btn)
+
+var start button = document.querySelector(start btn)
+var start button = document.querySelector(start btn)
+var show output = document.querySelector(.hidden
+    )
+
+
+
+
+
+
+
+
+
+var answer
+var answer
+var answer
+var answer
+var answer
+var answer
+var answer
